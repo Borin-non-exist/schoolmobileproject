980eeb282f253df793f7bba47b48a962d6e36ad7
Sok Borin <nhemsok7@gmail.com>
2024-02-15 16:34:03 +0700

    add MORE THINGS

---
+++ app/src/main/java/com/example/fassign/welcome/register/addphoto.java
@@ -0,0 +1,16 @@
+package com.example.fassign.welcome.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fassign.R;
+
+public class addphoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addphoto);
+    }
+}
